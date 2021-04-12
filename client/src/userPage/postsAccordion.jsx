6c63fed3e97b079d463053008b7a9df41e591c8f
Roei <roei.webd@gmail.com>
2021-04-12 17:27:53 +0300

    added emojies to user-posts

--- client/src/userPage/postsAccordion.jsx
+++ client/src/userPage/postsAccordion.jsx
@@ -3,19 +3,37 @@ import withAccordion from "../hoc/withAccordion";
 
 function Accordion({ items, isOpenIndex, handleClick, isClickedIndexes }) {
  
+  function getClassName(i){
+    if(isOpenIndex === i){
+      return " open"
+    } else if(isClickedIndexes.includes(i)){
+      return " close";
+    }
+    return "";
+  }
+
+  function returnEmojy(i){
+    if(isOpenIndex === i){
+      return <p className="post_accordion_emoji">👇</p>;
+    }
+    else{
+      return <p className="post_accordion_emoji">👈</p>
+    }
+   
+  }
 
   return items.map((item, i) => {
     return (
       <div key={i} className="post_accordion_item">
         <div onClick={() => handleClick(i)} className="title_container">
           <p>{item.title}</p>
+          {returnEmojy(i)}
         </div>
         <div
-          className={`details_container${
-            isOpenIndex === i ? " open" : isClickedIndexes.includes(i)?  " close": ""
-          }`}
+          className={`details_container${getClassName(i)}`}
         >
           <p className="description">{item.description}</p>
+
         </div>
       </div>
     );
